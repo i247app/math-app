10071095af7cec5fa4cfb5dd1bccd47aabb04899
Ky Nguyen <cuty0406@outlook.com>
2026-05-22 16:25:03 +0700

    remove unused screen

--- lib/features/onboarding/presentation/numi_home.dart
+++ lib/features/onboarding/presentation/numi_home.dart
@@ -8,7 +8,6 @@ import 'bloc/onboarding_cubit.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/otp_screen.dart';
-import 'screens/signup_prompt_screen.dart';
 import 'screens/signup_screen.dart';
 import 'screens/welcome_screen.dart';
 import 'widgets/app_background.dart';
