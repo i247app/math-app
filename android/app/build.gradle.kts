fbf5546999432846cc6b3d707b7539e4a888b893
Ky Nguyen <cuty0406@outlook.com>
2026-06-07 13:37:41 +0700

    remove wrong config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,10 +13,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.numi_flutter"
