007ced964b7caa2554cf9f0af7fc71a580e7168d
Ky Nguyen <cuty0406@outlook.com>
2026-06-08 15:22:26 +0700

    fix welcome screen not responsive

--- lib/features/onboarding/presentation/screens/welcome_screen.dart
+++ lib/features/onboarding/presentation/screens/welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -46,6 +48,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       child: ColoredBox(
         color: WelcomeScreen._background,
         child: SafeArea(
+          bottom: false,
           child: LayoutBuilder(
             builder: (context, constraints) {
               return SingleChildScrollView(
@@ -77,206 +80,242 @@ class _WelcomeComposition extends StatelessWidget {
 
   final VoidCallback onStart;
 
+  static const _designWidth = 390.0;
+  static const _designHeight = 800.0;
   static const _mascotAsset = 'assets/images/welcome_figma_mascot.png';
   static const _wavesAsset = 'assets/images/welcome_figma_waves.png';
   static const _booksAsset = 'assets/images/welcome_figma_books.png';
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.hardEdge,
-      children: [
-        const Positioned.fill(
-          child: ColoredBox(color: WelcomeScreen._background),
-        ),
-        const Positioned(
-          left: 0,
-          right: 0,
-          top: 75,
-          height: 284,
-          child: Center(
-            child: SizedBox(
-              width: 284,
-              height: 284,
-              child: Image(
-                image: AssetImage(_mascotAsset),
-                fit: BoxFit.cover,
-              ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final width = constraints.maxWidth;
+        final height = constraints.maxHeight;
+        final scale = math.max(width / _designWidth, height / _designHeight);
+        final canvasWidth = _designWidth * scale;
+        final canvasHeight = _designHeight * scale;
+        final leftOffset = (width - canvasWidth) / 2;
+        final topOffset = 0.0;
+
+        double s(double value) => value * scale;
+
+        return Stack(
+          clipBehavior: Clip.hardEdge,
+          children: [
+            const Positioned.fill(
+              child: ColoredBox(color: WelcomeScreen._background),
             ),
-          ),
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          top: 329,
-          height: 48,
-          child: Center(
-            child: SizedBox(
-              width: 211,
-              child: FittedBox(
-                fit: BoxFit.scaleDown,
-                alignment: Alignment.centerLeft,
-                child: Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'NUMI',
-                        style: GoogleFonts.bagelFatOne(
-                          color: WelcomeScreen._teal,
-                          fontSize: 40,
-                          height: 1,
+            Positioned(
+              left: leftOffset,
+              top: topOffset,
+              width: canvasWidth,
+              height: canvasHeight,
+              child: Stack(
+                clipBehavior: Clip.hardEdge,
+                children: [
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: s(75),
+                    height: s(284),
+                    child: Center(
+                      child: SizedBox(
+                        width: s(284),
+                        height: s(284),
+                        child: Image.asset(
+                          _mascotAsset,
+                          fit: BoxFit.cover,
                         ),
-                      ),
-                      TextSpan(
-                        text: 'NUMI',
-                        style: GoogleFonts.bagelFatOne(
-                          color: WelcomeScreen._coral,
-                          fontSize: 40,
-                          height: 1,
-                        ),
-                      ),
-                    ],
-                  ),
-                  maxLines: 1,
-                  softWrap: false,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          top: 385,
-          child: Center(
-            child: SizedBox(
-              width: 170,
-              child: Text.rich(
-                TextSpan(
-                  style: GoogleFonts.andika(
-                    color: WelcomeScreen._teal,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400,
-                    height: 22.5 / 18,
-                  ),
-                  children: [
-                    const TextSpan(text: 'Học tập '),
-                    TextSpan(
-                      text: 'thông minh\n',
-                      style: GoogleFonts.andika(
-                        color: WelcomeScreen._taglineCoral,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                        height: 22.5 / 18,
                       ),
                     ),
-                    const TextSpan(text: 'Tiến bộ mỗi ngày'),
-                  ],
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          top: 443,
-          height: 6,
-          child: Center(
-            child: SizedBox(
-              width: 48,
-              height: 6,
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  color: WelcomeScreen._taglineCoral,
-                  borderRadius: BorderRadius.circular(999),
-                ),
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          top: 425,
-          height: 375,
-          child: Center(
-            child: SizedBox(
-              width: 470,
-              height: 375,
-              child: Opacity(
-                opacity: 0.7,
-                child: ClipRect(
-                  child: Image.asset(
-                    _wavesAsset,
-                    fit: BoxFit.cover,
-                    alignment: Alignment.topCenter,
                   ),
-                ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: s(329),
+                    height: s(48),
+                    child: Center(
+                      child: SizedBox(
+                        width: s(211),
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          alignment: Alignment.centerLeft,
+                          child: Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: 'NUMI',
+                                  style: GoogleFonts.bagelFatOne(
+                                    color: WelcomeScreen._teal,
+                                    fontSize: s(40),
+                                    height: 1,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: 'NUMI',
+                                  style: GoogleFonts.bagelFatOne(
+                                    color: WelcomeScreen._coral,
+                                    fontSize: s(40),
+                                    height: 1,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            maxLines: 1,
+                            softWrap: false,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: s(385),
+                    child: Center(
+                      child: SizedBox(
+                        width: s(170),
+                        child: Text.rich(
+                          TextSpan(
+                            style: GoogleFonts.andika(
+                              color: WelcomeScreen._teal,
+                              fontSize: s(18),
+                              fontWeight: FontWeight.w400,
+                              height: 22.5 / 18,
+                            ),
+                            children: [
+                              const TextSpan(text: 'Học tập '),
+                              TextSpan(
+                                text: 'thông minh\n',
+                                style: GoogleFonts.andika(
+                                  color: WelcomeScreen._taglineCoral,
+                                  fontSize: s(18),
+                                  fontWeight: FontWeight.w400,
+                                  height: 22.5 / 18,
+                                ),
+                              ),
+                              const TextSpan(text: 'Tiến bộ mỗi ngày'),
+                            ],
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: s(443),
+                    height: s(6),
+                    child: Center(
+                      child: SizedBox(
+                        width: s(48),
+                        height: s(6),
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: WelcomeScreen._taglineCoral,
+                            borderRadius: BorderRadius.circular(999),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: s(425),
+                    height: s(375),
+                    child: Center(
+                      child: SizedBox(
+                        width: s(470),
+                        height: s(375),
+                        child: Opacity(
+                          opacity: 0.7,
+                          child: ClipRect(
+                            child: Image.asset(
+                              _wavesAsset,
+                              fit: BoxFit.cover,
+                              alignment: Alignment.topCenter,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: s(499),
+                    height: s(209),
+                    child: Center(
+                      child: SizedBox(
+                        width: s(361),
+                        height: s(209),
+                        child: ClipRect(
+                          child: Image.asset(
+                            _booksAsset,
+                            fit: BoxFit.cover,
+                            alignment: Alignment.topCenter,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: s(688),
+                    height: s(58),
+                    child: Center(
+                      child: SizedBox(
+                        width: s(230),
+                        height: s(58),
+                        child: _StartButton(
+                          onStart: onStart,
+                          scale: scale,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          top: 499,
-          height: 209,
-          child: Center(
-            child: SizedBox(
-              width: 361,
-              height: 209,
-              child: ClipRect(
-                child: Image.asset(
-                  _booksAsset,
-                  fit: BoxFit.cover,
-                  alignment: Alignment.topCenter,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          top: 688,
-          height: 58,
-          child: Center(
-            child: SizedBox(
-              width: 230,
-              height: 58,
-              child: _StartButton(onStart: onStart),
-            ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
   }
 }
 
 class _StartButton extends StatelessWidget {
-  const _StartButton({required this.onStart});
+  const _StartButton({
+    required this.onStart,
+    required this.scale,
+  });
 
   final VoidCallback onStart;
+  final double scale;
 
   @override
   Widget build(BuildContext context) {
+    final radius = BorderRadius.circular(20 * scale);
+
     return Material(
       color: WelcomeScreen._coral,
-      borderRadius: BorderRadius.circular(20),
+      borderRadius: radius,
       child: InkWell(
         onTap: () {
           HapticFeedback.lightImpact();
           onStart();
         },
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: radius,
         child: Center(
           child: Text(
             context.getText(AppKeys.start).toUpperCase(),
             style: GoogleFonts.fredoka(
               color: Colors.white,
-              fontSize: 18,
+              fontSize: 18 * scale,
               fontWeight: FontWeight.w700,
               height: 1.2,
             ),
